fed2f8cc884dfe4049623ec2909d1ffa0925a192
kskristof <kskristof2002@gmail.com>
2024-11-24 22:02:11 +0100

    Hibák kijavítása
    A felfedezett hibák és hiányosságok kijavítása megtörtént, ezért a kommentek ennél a verziónál eltávolításra kerültek.

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];    //új változó a program végén nem kerül törlésre, és a konstans nevéből hiányzik az aláhúzás
-    std::cout << '1-100 ertekek duplazasa'    //nem megfelelő idézőjel használata, hiányzó pontos vessző
-    for (int i = 0;)    //ciklus vége és lépésköze nincsenek megadva
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        b[i] = i * 2;    //az i helyett i+1 számot kell megszorozni 2-vel a helyes működés érdekében
+        b[i] = (i+1) * 2; 
     }
-    for (int i = 0; i; i++)    //itt is hiányzik a ciklus vége
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        std::cout << "Ertek:"    //így még nem írja ki az értékeket, illetve célszerű valamennyit helyet hagyni a kiiratások között, és itt sincs pontos vessző
+        std::cout << "Ertek:" << b[i] << "  "; 
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;    //egy sortörést érdemes beszúrni az előző felsorolás után
-    int atlag;    //ennek a változónak nincs kezdőértéke
-    for (int i = 0; i < N_ELEMENTS, i++)    //vessző helyett pontos vesszőt kell itt használni a lépésköz előtt
+    std::cout << std::endl << "Atlag szamitasa: " << std::endl; 
+    int atlag=0; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i]    //nincs pontos vessző
+        atlag += b[i];  
     }
     atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;    //a 7. sorban említett változót ezután érdemes törölni
+    std::cout << "Atlag: " << atlag << std::endl;  
+    delete b;
     return 0;
 }
